bb88c1380c6ecfeb52fe5183089f7e7f5fea418e
qinzhen3 <qinzhen3@xiaomi.com>
2020-12-27 00:54:33 +0800

    ceshi

--- src/controller/userInfo.ts
+++ src/controller/userInfo.ts
@@ -3,6 +3,7 @@ import { userInfo } from '../entity/userInfo';
 import { getRepository } from 'typeorm';
 
 export async function setUserInfo(req: Request, res: Response) {
+    //ceshi
     const entity = await getRepository(userInfo).create(req.body);
     await getRepository(userInfo)
         .save(entity)
