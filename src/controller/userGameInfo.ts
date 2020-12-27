839362daa5e234f5986b0aefcd5ff444f561e5eb
qinzhen3 <qinzhen3@example.com>
2020-12-27 19:00:42 +0800

    fix：修复异常处理

--- src/controller/userGameInfo.ts
+++ src/controller/userGameInfo.ts
@@ -6,18 +6,16 @@ export async function setUserGameInfo(req: Request, res: Response) {
     const entity = await getRepository(userGameInfo).create(req.query);
     await getRepository(userGameInfo)
         .save(entity)
-        .then((r) =>
-            res.json({
-                status: '200',
-                data: r
-            })
-        )
-        .catch((error) => res.json(error));
+        .then((r) => res.json(r))
+        .catch((error) => {
+            throw new Error(error);
+        });
 }
 
 export async function getUserGameInfo(req: Request, res: Response) {
     await getRepository(userGameInfo)
         .findOne(req.query)
+
         .then((r) =>
             res.json({
                 status: '200',
--- src/controller/userInfo.ts
+++ src/controller/userInfo.ts
@@ -6,24 +6,17 @@ export async function setUserInfo(req: Request, res: Response) {
     const entity = await getRepository(userInfo).create(req.query);
     await getRepository(userInfo)
         .save(entity)
-        .then((r) =>
-            res.json({
-                status: '200',
-                data: r
-            })
-        )
-        .catch((error) => res.json(error));
+        .then((r) => res.json(r))
+        .catch((error) => {
+            throw new Error(error);
+        });
 }
 
 export async function getUserInfo(req: Request, res: Response) {
     await getRepository(userInfo)
         .findOne(req.query)
-        .then((r) =>
-            res.json({
-                status: '200',
-                data: r || [],
-                errorMsg: ''
-            })
-        )
-        .catch((error) => res.json(error));
+        .then((r) => res.json(r || null))
+        .catch((error) => {
+            throw new Error(error);
+        });
 }
